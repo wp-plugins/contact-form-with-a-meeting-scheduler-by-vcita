1373c729cb9d98369b6ca96857d7ccaa0ea5a5b4
erel <erel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-16 12:53:06 +0000

    Update tags to ver 3.0
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-with-a-meeting-scheduler-by-vcita/trunk@653575 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vcita-contact-form.php
+++ vcita-contact-form.php
@@ -3,7 +3,7 @@
 Plugin Name: Contact Form by vCita
 Plugin URI: http://www.vcita.com
 Description: Contact form by vCita proves to increase the number of contact form requests 
-Version: 2.1.3
+Version: 3.0
 Author: vCita.com
 Author URI: http://www.vcita.com
 */
