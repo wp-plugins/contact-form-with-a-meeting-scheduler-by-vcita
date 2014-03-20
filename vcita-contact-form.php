952a6d9aae8e906e3274715c8b6a18c3f14a8ee1
warolv <warolv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-20 15:06:16 +0000

    Update to 3.2.1 version main php file
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-with-a-meeting-scheduler-by-vcita/tags/3.2.1@878828 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vcita-contact-form.php
+++ vcita-contact-form.php
@@ -3,7 +3,7 @@
 Plugin Name: Contact Form by vCita
 Plugin URI: http://www.vcita.com
 Description: Contact form by vCita proves to increase the number of contact form requests 
-Version: 3.2.0
+Version: 3.2.1
 Author: vCita.com
 Author URI: http://www.vcita.com
 */
